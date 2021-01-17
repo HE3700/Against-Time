c63d3c4fbb05cd24a175a252b0e913a44bbeaf69
HE3700 <77581866+HE3700@users.noreply.github.com>
2021-01-17 18:26:15 +0100

    1
    pálya 90%-a kész
    kamera forgatható

---
+++ Against Time by HorvathErik/Assets/Scripts/mouseLook.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseLook : MonoBehaviour
+{
+    //KAMERAMOZGÁSOK
+
+    public float mouseSens = 100f;
+    public Transform playerBody;
+
+    float xRotation = 0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSens * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSens * Time.deltaTime;
+
+        playerBody.Rotate(Vector3.up * mouseX);
+
+        //felfelé, lefelé nézés korlátozása
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+
+    }
+}
